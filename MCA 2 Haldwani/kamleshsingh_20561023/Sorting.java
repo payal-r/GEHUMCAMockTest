2542f2c483cd962c05d582c54d1d2177e6268599
kamlesh singh <30825138+kamlesh78@users.noreply.github.com>
2021-06-14 13:46:50 +0530

    Update Sorting.java

--- MCA 2 Haldwani/kamleshsingh_20561023/Sorting.java
+++ MCA 2 Haldwani/kamleshsingh_20561023/Sorting.java
@@ -1,1 +1,27 @@
 
+import java.util.*;
+import java.util.Scanner;
+import java.util.Arrays;
+public class Sorting{
+
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter size of array");
+int size = sc.nextInt();
+int[] arr = new int[size];
+System.out.println("Enter Elements of the array");
+
+for(int i=0;i<size;i++){
+arr[i] = sc.nextInt();
+}
+
+Arrays.sort(arr);
+System.out.println("Sorted array is: ");
+for(int i=0;i<size;i++){
+ System.out.println(arr[i]);
+}
+
+
+}
+
+}
