56e86cd8687ff420e385e4772decc3e97be3e00e
AshuJoshi001 <47688714+AshuJoshi001@users.noreply.github.com>
2021-06-14 13:32:57 +0530

    Add files via upload

---
+++ MCA 2 Haldwani/PrajwalJoshi_20711056/demotest.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class demotest {    
+    public static void main(String[] args) {        
+               
+        int n;  
+        Scanner sc=new Scanner(System.in);  
+        System.out.print("Enter the number of elements you want to store: ");    
+        n=sc.nextInt();   
+        int[] arr = new int[10];  
+        System.out.println("Enter the elements of the array: ");  
+        for(int i=0; i<n; i++)  
+        {  
+        arr[i]=sc.nextInt();  
+        } 
+        
+        int temp = 0;   
+            
+        System.out.println("Elements of original array: ");    
+        for (int i = 0; i < arr.length; i++) {     
+            System.out.print(arr[i] + " ");    
+        }    
+              
+        for (int i = 0; i < arr.length; i++) {     
+            for (int j = i+1; j < arr.length; j++) {     
+               if(arr[i] > arr[j]) {    
+                   temp = arr[i];    
+                   arr[i] = arr[j];    
+                   arr[j] = temp;    
+               }     
+            }     
+        }    
+          
+        System.out.println();    
+            
+        System.out.println("Elements of array sorted in ascending order: ");    
+        for (int i = 0; i < arr.length; i++) {     
+            System.out.print(arr[i] + " ");    
+        }    
+    }    
+} 
